2388f2e28b3e69212371b5e9c230b66860a908f3
yujidate990 <yuji.date.gh@gmail.com>
2020-11-22 09:53:18 +0900

    改行忘れ修正

--- src/main/java/com/example/demo/trySpring/HelloRepository.java
+++ src/main/java/com/example/demo/trySpring/HelloRepository.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 /**
- * ④：dbへのCRUDを行い
+ * ④：dbへのCRUDを行い<br>
  * ⑤：結果を返却するクラス
  * @author datey
  */
